3b4f668f9d9284133a8c06d6eb4b9081b9d3e48b
Your Name <you@example.com>
2023-05-11 14:29:18 +0900

    first commit

--- src/pages/Lesson01/index.js
+++ src/pages/Lesson01/index.js
@@ -4,11 +4,56 @@ import Chart from "../../components/Chart01";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return input.map(([name, count]) => ({//inputのname, countを仮引数
+  /*return input.map(([name, count]) => ({//inputのname, countを仮引数
     name,//name:name 省略されてて、こう書ける
     count,//count:count
-  }));
+  }));*/
+
+  /*
+  const f = ([a,b]) => ({
+    name:a,
+    count:b,
+  })
+  console.log(input.map(f));
+  return input.map(f);
+  */
+
+  /*//オブジェクトショートハンド
+  const f = (row) => ({
+    name:row[0],
+    count:row[1],
+  })
+  console.log(input.map(f));
+  return input.map(f);*/
+
+  //functionを使って
+  function f(row){
+    const a = {
+      name:row[0],
+      count:row[1]
+    }//{[]} []内がプロパティ,nameなどがプロパティ名、
+    //全体{}がオブジェクト
+    return a;
+    //※return a, bb;最後(bb)しか返せない
+
+  }
+  console.log(input.map(f));
+  return input.map(f);
 };
+//mapは関数を受け取るメソッド、新しい配列を生成するもの
+/*
+const f = (row) => ({
+  row.name,
+  row.count,
+})
+
+const f = ([name, count]) => ({
+  name,
+  count,
+})
+coust ans = input.map(f);
+fがcallbackFn, arrがinput, ansが
+*/
 /*
 inputをmapする。
 nameとcountを1要素ずつ見ていく(foreachみたいなやつ)
